415b41f5b07cb4869229b3f2d40ec7ed11293888
MeninoDoBK <bernardohd12@gmail.com>
2026-07-16 21:53:43 -0300

    Primeiro Commit

---
+++ 01-basico-python/conteudo/03-operadores-comparação.py
@@ -0,0 +1,12 @@
+# Exemplos
+a = 20
+b = 15
+
+# Operadores de comparação
+
+print(a >= b)# Maior igual
+print(a <= b)# Menor igual
+print(a != b)# Diferente
+print(a == b)# Igual
+print(a > b)# Maior
+print(a < b)# Menor
---
+++ 01-basico-python/conteudo/04-operadores lógicos.py
@@ -0,0 +1,19 @@
+# Exemplos
+idade = 14
+altura = 1.55
+
+# Operador and
+resultado = (idade > 18) and (altura > 1.78)
+print(resultado)
+
+# Operador or
+porta = 'a'
+janela = 'f'
+alarme = (porta == 'a') or (janela == 'a')
+print('O alarme foi disparado:',alarme)
+
+# Operador not
+tem_dinheiro = False
+tem_dinheiro = not tem_dinheiro
+mensagem = 'Tem dinheiro? ',tem_dinheiro
+print(mensagem)                                                                                                                                      
